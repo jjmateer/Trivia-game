4d082e4ad3f5f6ef088f15d6fa604224a73dc806
John Mateer <jj.mateer@live.com>
2019-07-16 23:08:17 -0700

    Added on click functions to correct and incorrect buttons that increment the score. After those inputs, the start button, or the submit button have been pressed, they will become disabled. When the submit button is pressed, it appends the correct answers and incorrect answers to the scorecount div.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,16 @@
-
+$("#inputForm").hide();
 var timerInterval;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unanswered = 0;
-var questionCorrectAnswer;
 var timeLeft = 30;
-var questionNumber = 0;
 var clockRunning = false;
 $("#resetButton").hide();
-
-
-    $("#question1").text("What fast food chain sells the whopper?");
-
-    $("#question2").text("What fast food chain sells the big mac?");
-    $("#question3").text("What fast food chain sells the triple layer nacho?");
-    $("#question4").text("What fast food chain sells animal fries?");
-    $("#question5").text("What fast food chain sells kentucky fried chicken?");
-
-    $("#btn11")
-
-
-
-
-
-    var correctChoices = [
-    "Burger King",
-    "Mcdonalds",
-    "Taco Bell",
-    "In-N-Out",
-    "KFC"
-];
-//Choices for questions
-var questionChoices = [
-    ["Mcdonalds", "Carl's Jr.", "Burger King", "Taco Bell"],
-    ["Mcdonald's", "Taco Bell", "Carl's Jr.", "Burger King"],
-    ["Burger King", "Carl's Jr.", "Mcdonald's", "Taco Bell"],
-    ["In - N - Out", "Burger King", "Carl's Jr.", "Taco Bell"],
-    ["Carl's Jr.", "Burger King", "KFC", "Taco Bell"]
-];
-
-
-
-
-
-
-
+$("#question1").text("What fast food chain sells the whopper?");
+$("#question2").text("What fast food chain sells the big mac?");
+$("#question3").text("What fast food chain sells the triple layer nacho?");
+$("#question4").text("What fast food chain sells animal fries?");
+$("#question5").text("What fast food chain sells kentucky fried chicken?");
 function timeConverter(t) {
     let minutes = Math.floor(t / 60);
     let seconds = t - (minutes * 60)
@@ -59,61 +25,49 @@ function timeConverter(t) {
     }
     return `${minutes}:${seconds}`;
 }
-
 function countdownTimer() {
     timeLeft--;
     let convertedTime = timeConverter(timeLeft);
-    $("#timeRemaining").html(convertedTime);
+    $("#timeRemaining").html("Time remaining: " + convertedTime);
     if (timeLeft === 0) {
         unanswered++;
-        questionNumber++;
         timeout();
         clearInterval(timerInterval);
     }
 }
-
-
 function startTimer() {
     clockRunning = true;
     timerInterval = setInterval(countdownTimer, 1000);
 }
-
-
-
 function timeout() {
     clearInterval(timerInterval);
     $("#startButton").text("Time out!!");
 
-
-
-
-
 }
-
-
-
-$("#submit").on("click", function () {
-
-
-
-if (userChoice === questionCorrectAnswer) {
- 
-        correctAnswers++;
-
-
-    } else {
-
-        incorrectAnswers++;
-
-    }
+$("#input13, #input23, #input33, #input42, #input51").on("click", function () {
+correctAnswers++;
+console.log(correctAnswers);
+$(this).prop('disabled', true);
 
 });
+$("#input11, #input12, #input21, #input22, #input31, #input32, #input41, #input43, #input52, #input53").on("click", function() {
+    incorrectAnswers++;
+    console.log(incorrectAnswers)
+    $(this).prop('disabled', true);
 
+});
 $("#startBtn").on("click", function () {
+    $("#inputForm").show();
     startTimer();
-    $("#resetBtn").hide();
-    $("#startBtn").hide();
-
-
+    $(this).prop('disabled', true);
+});
+$("#submit").on("click", function() {
+    $("#inputForm").empty();
+    $("#scoreCount").append("Correct answers: " + correctAnswers + "   ");
+    $("#scoreCount").append("Incorrect answers: " + incorrectAnswers + "   ");
+    $("#scoreCount").append("Unanswered: " + unanswered + "   ");
+    clearInterval(timerInterval);
+    $(this).prop('disabled', true);
 
 });
+
