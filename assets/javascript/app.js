ce001b2a5c93ae94645e041a38ba05cc790ae798
John Mateer <jj.mateer@live.com>
2019-07-16 19:52:39 -0700

    Going to use this to do the easier activity

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 
 var timerInterval;
-var correctAnswer = 0;
-var incorrectAnswer = 0;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
 var unanswered = 0;
 var questionCorrectAnswer;
 var timeLeft = 15;
@@ -9,60 +9,60 @@ var questionNumber = 0;
 var clockRunning = false;
 var questionChoiceshtml = [];
 // $("#resetButton").hide();
-//Array of questions
-var questions = [
-    "What fast food chain sells the whopper?",
-    "What fast food chain sells the big mac?",
-    "What fast food chain sells the triple layer nacho?",
-    "What fast food chain sells animal fries?",
-    "What fast food chain sells kentucky fried chicken?"
-];
-//Correct answers
-var correctChoices = [
-    "Burger King",
-    "Mcdonalds",
-    "Taco Bell",
-    "In-N-Out",
-    "KFC"
-];
-//Choices for questions
-var questionChoices = [
-    ["Mcdonalds", "Carl's Jr.", "Burger King", "Taco Bell"],
-    ["Mcdonald's", "Taco Bell", "Carl's Jr.", "Burger King"],
-    ["Burger King", "Carl's Jr.", "Mcdonald's", "Taco Bell"],
-    ["In - N - Out", "Burger King", "Carl's Jr.", "Taco Bell"],
-    ["Carl's Jr.", "Burger King", "KFC", "Taco Bell"]
-];
+// //Array of questions
+// var questions = [
+//     "What fast food chain sells the whopper?",
+//     "What fast food chain sells the big mac?",
+//     "What fast food chain sells the triple layer nacho?",
+//     "What fast food chain sells animal fries?",
+//     "What fast food chain sells kentucky fried chicken?"
+// ];
+// //Correct answers
+// var correctChoices = [
+//     "Burger King",
+//     "Mcdonalds",
+//     "Taco Bell",
+//     "In-N-Out",
+//     "KFC"
+// ];
+// //Choices for questions
+// var questionChoices = [
+//     ["Mcdonalds", "Carl's Jr.", "Burger King", "Taco Bell"],
+//     ["Mcdonald's", "Taco Bell", "Carl's Jr.", "Burger King"],
+//     ["Burger King", "Carl's Jr.", "Mcdonald's", "Taco Bell"],
+//     ["In - N - Out", "Burger King", "Carl's Jr.", "Taco Bell"],
+//     ["Carl's Jr.", "Burger King", "KFC", "Taco Bell"]
+// ];
 
 
 
 //make an array displaying the questions, answers, and correct answers
 var questionKey = [
     {
-        "question": questions[0],
-        "choices": questionChoices[0],
-        "correct": correctChoices[0]
+        "question": "What fast food chain sells the whopper?",
+        "choices": ["Mcdonalds", "Carl's Jr.", "Burger King", "Taco Bell"],
+        "correct": "Burger King"
     },
     {
-        "question": questions[1],
-        "choices": questionChoices[1],
-        "correct": correctChoices[1]
+        "question": "What fast food chain sells the big mac?",
+        "choices": ["Mcdonald's", "Taco Bell", "Carl's Jr.", "Burger King"],
+        "correct":"Mcdonalds" 
     },
     {
-        "question": questions[2],
-        "choices": questionChoices[2],
-        "correct": correctChoices[2]
+        "question": "What fast food chain sells the triple layer nacho?",
+        "choices": ["Burger King", "Carl's Jr.", "Mcdonald's", "Taco Bell"],
+        "correct": "Taco Bell"
     },
     {
-        "question": questions[3],
-        "choices": questionChoices[3],
-        "correct": correctChoices[3]
+        "question": "What fast food chain sells animal fries?",
+        "choices": ["In - N - Out", "Burger King", "Carl's Jr.", "Taco Bell"],
+        "correct": "In-N-Out"
     },
     {
-        "question": questions[4],
-        "choices": questionChoices[4],
-        "correct": correctChoices[4]
-    },
+        "question": "What fast food chain sells kentucky fried chicken?",
+        "choices": ["Carl's Jr.", "Burger King", "KFC", "Taco Bell"],
+        "correct": "KFC"
+    }
 ]
 
 function displayQuestion() {
@@ -72,15 +72,27 @@ function displayQuestion() {
 }
 
 function displayChoices() {
-    for (var i = 0; i < questionChoices[0].length; i++) {
-        questionChoiceshtml.push('<button id="choices">' + questionChoices[0][i] + '</button>');
-        console.log(Object.keys(questionKey[i]));
+    for (i = -1; i < questionKey[questionNumber].choices.length; i++) {
+        $("#userChoices").append(`<div class="choices"><input type="radio" name="choices" 
+        value="${questionKey[questionNumber].choices[i]}"> ${questionKey[questionNumber].choices[i]}</div>`);
     }
-    $("#userChoices").html(questionChoiceshtml);
-    $("#choices").on("click", function() {
-        evaluate();
-        });
 }
+
+// function displayChoices() {
+//     for (var i = -1; i < questionKey[questionNumber].answers; i++) {
+//         questionChoiceshtml.push('<btn id="choices">' + questionChoices[i] + '</btn>');
+//     }
+//     for (var i = -1; i < questionChoices; i++) {
+//         $("#userChoices").html(questionChoiceshtml[i]);
+//         $("#choices").on("click", function () {
+//             evaluate();
+//             questionNumber++;
+//             $("#choices")
+//         });
+//     }
+
+
+// }
 
 function timeConverter(t) {
     let minutes = Math.floor(t / 60);
@@ -105,7 +117,7 @@ function countdownTimer() {
         unanswered++;
         questionNumber++;
         timeout();
-        resetTimer();
+        reset();
     }
 }
 
@@ -127,10 +139,9 @@ function timeout() {
 
     clearInterval(timerInterval);
     $("#startButton").text("Time out!!");
-    $("#triviaQuestion").text("The correct answer was: " + correctAnswer);
-    $("#gifDiv").empty();
-    $('#gifDiv').html('<img src="assets/images/burgerkinggif.webp" />');
-    setTimeout(function(){reset}, 3000);
+    $("#triviaQuestion").text("The correct answer was: " + correct);
+    $("#correctdiv").empty();
+    setTimeout(function () { reset }, 3000);
 
 
 }
@@ -142,44 +153,38 @@ function reset() {
         $("triviaquestion").empty();
         $("#userChoices").empty();
         $(".timeRemaining").empty();
-        $("userchoices").html(`
-        <div>Number of correct answers: ${correctAnswers}</div>
-        <div>Number of wrong answers: ${incorrectAnswers}</div>
-        `);
+        $("userchoices").html(correctAnswers + incorrectAnswers + unanswered);
 
     }
     else {
-        resetTimer()
+        resetTimer();
         $("#triviaQuestion").empty();
-        displayChoices()
+        displayChoices();
         $("#userchoices").empty();
         displayQuestion();
         questionCorrectAnswer = questionKey[questionNumber].correct;
         startTimer();
         questionNumber++;
-        console.log("Correct answer: " + currentCorrectAnswer);
+        $("#correctdiv").text("Correct answer: " + questionCorrectAnswer);
     }
 }
 
-function evaluate() {
 
-    var radios = document.getElementsByName("choices");
-    var trueFalse = false;
+$("#nextBtn").on("click", function () {
+    var qChoices = document.getElementsByName("choices");
+    let trueFalse = false;
     var userChoice;
-    for (i = 0; i < radios.length; i++) {
+    $("#userChoices").empty();
+    for (i = 0; i < qChoices.length; i++) {
 
-        if (radios[i].checked) {
+        if (qChoices[i].checked) {
             trueFalse = true;
-            userChoice = radios[i].value;
+            userChoice = qChoices[i].value;
         }
     }
-    if (!trueFalse) {
-        alert("timeout!!");
-        for (let i = 0; i > timeLeft; i++) { }
-        unanswered++;
-        reset();
-    }
-    else if (userChoice === questionCorrectAnswer) {
+
+
+    if (userChoice === questionCorrectAnswer) {
         alert("correct!");
         correctAnswers++;
         questionNumber++;
@@ -190,18 +195,16 @@ function evaluate() {
         questionNumber++;
         reset();
     }
-}
 
-$("#nextBtn"). on("click", function() {
-    
 });
-$("#startBtn").on("click", function() {
+
+$("#startBtn").on("click", function () {
     displayQuestion();
     displayChoices();
     startTimer();
     $("#resetBtn").hide();
     $("#startBtn").hide();
-    $("#nextBtn").hide();
+
 
 
 });
